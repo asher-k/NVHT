988fab590c8a457af2430dd9e0efc517d9e999b6
Asher Stout <57604445+asher-k@users.noreply.github.com>
2023-10-14 18:07:32 +1300

    Added unformatted tooltip and better readability in graph

--- code/viz.R
+++ code/viz.R
@@ -2,13 +2,15 @@ library(igraph)
 library(networkD3)
 library(scales)
 library(stringi)
+library(plyr)
 
-# First reformat toponym counts
+# First reformat Toponym counts
 ecs <- entry_counts 
 ecs$Type <- gsub(".+:", "", ecs$Toponym)
+ecs$Type <- mapvalues(ecs$Type, c("cho", "hyd", "oro"), c("Choronym", "Hydronym", "Oronym"))
 ecs$Toponym <- stri_unescape_unicode(gsub("<U\\+(....)>", "\\\\u\\1", ecs$Toponym))
 
-# Function for assigning relationships' toponym ids instead of strings
+# Function for assigning relationships' Toponym ids instead of strings
 update_func <- function(x){
   k <- which(ecs$Toponym == x)
   return(k-1)
@@ -21,28 +23,72 @@ rcs$From <- stri_unescape_unicode(gsub("<U\\+(....)>", "\\\\u\\1", rcs$From))
 rcs$To <- as.array(lapply(rcs$To, update_func))
 rcs$From <- as.array(lapply(rcs$From, update_func))
 
-# Rescale frequency counts for entries/relationships to scale better with the figure
+# Rescale frequency counts for entries/relationships to scale better with the figure (optional)
 ecs$Toponym <- gsub(":[a-zA-Z]*", "", ecs$Toponym)  # remove :cat from Toponyms
 # rcs$total_count <- rescale(rcs$total_count, t=c(1,5))
 # ecs$Freq <- rescale(ecs$Freq, t=c(1,5))
 
 # Define the network graph
-ColourScale <- 'd3.scaleOrdinal().domain(["cho", "hyd", "oro"]).range(["#cc189f", "#2566e8", "#db8851"]);'
+ColourScale <- 'd3.scaleOrdinal().domain(["Choronym", "Hydronym", "Oronym"]).range(["#cc189f", "#2566e8", "#db8851"]);'
 
 n <- forceNetwork(Links=rcs, Source="From", Target="To", Value="total_count", 
              Nodes=ecs, NodeID="Toponym", Group="Type", Nodesize="Freq",
-             colourScale = ColourScale, linkDistance = 200, charge = -30, zoom=T,
-             fontSize = 20, fontFamily = "serif", linkColour = "#666", opacity=0.9, opacityNoHover = TRUE, legend=T,
+             colourScale = ColourScale, linkDistance = 80, charge = -20, zoom=T,
+             fontSize = 20, fontFamily = "serif", linkColour = "#666", opacity=0.9, opacityNoHover = TRUE, legend=T
 )
 
 # Widget to make interaction with graph smoother
 n <- htmlwidgets::onRender(n, jsCode =
   'function(el, x) {
+    // Additional default settings
     d3.selectAll(".node text").style("font-weight", "bold");
-    d3.selectAll(".node text").style("fill", "black")
+    d3.selectAll(".node text").style("stroke", "black");
+    d3.selectAll(".node text").attr("stroke-width", "0");
     d3.selectAll(".link").style("opacity", "0.2");
     
-    d3.selectAll(".legend text").text("Choronym");
+    // Moving onto/off Nodes 
+    d3.selectAll(".node").on("mouseenter", function(e, d){
+      d3.selectAll(".node text").style("font-size", "20");
+      d3.selectAll(".node text").style("stroke-width", "0");
+      d3.select(this).select("text").style("font-size", "36");
+      d3.select(this).select("text").style("stroke-width", "2");
+      
+      // Update tooltip
+      d3.select("#tooltip").style("left", d3.event.pageX - 60 + "px").style("top", d3.event.pageY + 20 + "px");
+      d3.select("#tooltip").style("opacity", 1).text(d);
+    });
+    d3.selectAll(".node").on("mouseleave", function(e, d){
+      d3.selectAll(".node text").style("stroke-width", "0");
+      d3.selectAll(".node text").style("font-size", "20");
+      d3.selectAll(".link").style("opacity", "0.2");
+      
+      // Update tooltip
+      d3.select("#tooltip").style("opacity", 0);
+    });
+    d3.selectAll(".node").on("mousemove", function() {
+      d3.select("#tooltip").style("left", d3.event.pageX - 60 + "px").style("top", d3.event.pageY + 20 + "px");
+    })
+    
+    // Moving onto/off Linkages 
+    d3.selectAll(".link").on("mouseenter", function(e, d){
+      d3.select(this).style("opacity", "0.9");
+      
+      // Update tooltip
+      d3.select("#tooltip").style("left", d3.event.pageX - 60 + "px").style("top", d3.event.pageY + 20 + "px");
+      d3.select("#tooltip").style("opacity", 1).text(d);
+    });
+    d3.selectAll(".link").on("mouseleave", function(e, d){
+      d3.select(this).style("opacity", "0.2");
+      
+      // Update tooltip
+      d3.select("#tooltip").style("opacity", 0);
+    });
+    d3.selectAll(".link").on("mousemove", function() {
+      d3.select("#tooltip").style("left", d3.event.pageX - 60 + "px").style("top", d3.event.pageY + 20 + "px");
+    })
+    
+    // Setup Hover Tooltip
+    d3.select("body").append("div").attr("id", "tooltip").attr("style", "position: absolute; opacity: 0;");
   }'
 )
 
